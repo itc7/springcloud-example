ea10edd07719e0d2afafd448edba3270dbae4851
itc7 <fpj258@gmail.com>
2017-04-17 14:42:28 +0800

    计算节点

---
+++ ex-compute/src/main/java/cn/itc7/sc/compute/ComputeApplication.java
@@ -0,0 +1,17 @@
+package cn.itc7.sc.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by fpj on 2017/04/17.
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ComputeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputeApplication.class,args);
+    }
+}
